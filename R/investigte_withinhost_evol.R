8d6d9675dc7ead25f04fe89c95853d113a218e99
thkuo <tku16@helmholtz-hzi.de>
2020-04-04 17:14:10 +0200

    transmission tree and within-host trees achieved

--- R/investigte_withinhost_evol.R
+++ R/investigte_withinhost_evol.R
@@ -9,86 +9,28 @@ w.scale=0.1
 pi=0.75
 dateT= 2008
 dateStartOutbreak=2005
-# simu <- simulateOutbreak(
-#     neg=neg,pi=pi,off.r=off.r,w.shape=w.shape,
-#     w.scale=w.scale,dateStartOutbreak=dateStartOutbreak,dateT=dateT)
-# ttree<-extractTTree(simu)
-# plotTTree(ttree,w.shape,w.scale)
 
-## Then, review the function
-## and see how the within-host tree is made
-source('./TransPhylo/R/withinhost.R')
-#' Simulate an outbreak
-#' @param off.r First parameter of the negative binomial distribution for offspring number
-#' @param off.p Second parameter of the negative binomial distribution for offspring number
-#' @param neg the within-host effective population size (Ne) timesgeneration duration (g)
-#' @param nSampled number of sampled infected individuals, or NA for any
-#' @param pi probability of sampling an infected individual
-#' @param w.shape Shape parameter of the Gamma probability density function representing the generation time
-#' @param w.scale Scale parameter of the Gamma probability density function representing the generation time 
-#' @param ws.shape Shape parameter of the Gamma probability density function representing the sampling time
-#' @param ws.scale Scale parameter of the Gamma probability density function representing the sampling time 
-#' @param w.mean Mean of the Gamma distribution representing the generation time
-#' @param w.std Std of the Gamma distribution representing the generation time 
-#' @param ws.mean Mean of the Gamma distribution representing the sampling time
-#' @param ws.std Std of the Gamma distribution representing the sampling time 
-#' @param dateStartOutbreak Date when index case becomes infected
-#' @param dateT Date when process stops (this can be Inf for fully simulated outbreaks)
-#' @return Combined phylogenetic and transmission tree
-#' @export
-# simulateOutbreak = function(off.r=1,off.p=0.5,neg=0.25,nSampled=NA,pi=0.5,w.shape=2,w.scale=1,ws.shape=NA,ws.scale=NA,w.mean=NA,w.std=NA,ws.mean=NA,ws.std=NA,dateStartOutbreak=2000,dateT=Inf) {
-off.p=0.5
-ws.shape=NA
-ws.scale=NA
-w.mean= NA
-w.std= NA
-ws.mean= NA
-ws.std= NA
-nSampled= NA
+source('simulateOutbreak_unglued.R')
+t_w_trees <- simulateOutbreak_unglued(
+    neg=neg,pi=pi,off.r=off.r,w.shape=w.shape,
+    w.scale=w.scale,dateStartOutbreak=dateStartOutbreak,dateT=dateT)
+plotTTree2(list(ttree= t_w_trees$ttree, nam= t_w_trees$nam))
 
-    if (!is.na( w.mean)&&!is.na( w.std)) { w.shape= w.mean^2/ w.std^2; w.scale= w.std^2/ w.mean}
-    if (!is.na(ws.mean)&&!is.na(ws.std)) {ws.shape=ws.mean^2/ws.std^2;ws.scale=ws.std^2/ws.mean}
-    if (is.na(ws.shape)) ws.shape=w.shape
-    if (is.na(ws.scale)) ws.scale=w.scale
-    
-    #Create a transmission tree with nSampled infected sampled individuals
-    nsam<-0
-    nh<-0
-    rejected=-1
-    while (is.na(nSampled)||nsam!=nSampled) {
-        ttree=NULL
-        while (is.null(ttree)) {
-            mtt<-makeTTree(off.r,off.p,pi,w.shape,w.scale,ws.shape,ws.scale,dateT-dateStartOutbreak,nSampled)
-            rejected=rejected+1
-            ttree<-mtt$ttree
-            probttree<-mtt$prob
-            if (mtt$pruned>0) {
-                dateStartOutbreak=dateStartOutbreak+mtt$pruned
-                cat(sprintf('Note that simulated outbreak was pruned: in order to have %d sampled by present date %f, the start date was set to %f\n',nSampled,dateT,dateStartOutbreak))
-            }
-        }
-        nsam<-length(which(!is.na(ttree[,2])))
-        nh=nrow(ttree)-nsam
-        if (is.na(nSampled)) nSampled=nsam
-    }
-    if (rejected>0) cat(sprintf('Note that rejection sampling was used %d times to simulate outbreak with %d sampled individuals\n',rejected,nSampled))
-    n<-nsam+nh
-    
-    #Create a within-host phylogenetic tree for each infected host
-    wtree<-vector('list',n)
-    probwithin=0
-    for (i in (1:n)) {
-        if (is.na(ttree[i,2])) {times<-c(           ttree[which(ttree[,3]==i),1])-ttree[i,1]}
-        else {times<-c(ttree[i,2],ttree[which(ttree[,3]==i),1])-ttree[i,1]}
-        a<-withinhost(times,neg)
-        wtree[[i]]=a$nodes
-        probwithin=probwithin+a$prob
-    }
-    
-    #Glue these trees together
-    truth<-.glueTrees(ttree,wtree)
-    truth[,1]<-truth[,1]+dateStartOutbreak
-    l=list(ctree=truth,nam=mtt$nam,probttree=probttree,probwithin=probwithin)
-    class(l)<-'ctree'
-    return(l)
-# }  
+## save the trees
+## don't forget the node indices
+output_d<- 'outbreak_sim'
+dir.create(file.path(output_d), recursive = T)   
+## the transmission tree
+ttree_f<- sprintf('%s/ttree.mat', output_d)
+write.table(t_w_trees$ttree, file = ttree_f, sep = '\t',  col.names = F)
+## save the within-host trees
+output_wtrees_d<- sprintf('%s/wtrees', output_d)
+dir.create(file.path(output_wtrees_d), recursive = T)   
+library(ape)
+for (patient_ix in 1:length(t_w_trees$wtrees)){
+    # patient_ix<- 1
+    ptree<- list(ptree= t_w_trees$wtrees[[patient_ix]], nam= t_w_trees$nam)
+    dtr<- phyloFromPTree(ptree)
+    dtr_f<- sprintf('%s/%s.nwk', output_wtrees_d, patient_ix)
+    write.tree(phy= dtr, file = dtr_f)
+}
--- README.md
+++ README.md
@@ -5,17 +5,21 @@ genome simulator + transmission simulation
 - output: genomes, transmission tree, and time-measured tree of sampled cases
 
 ## Idea
-- transphylo: big tree where each node is a small coalescence tree
-- alfsim: 
-    - fixed parameters (mutation rates, substitution model...etc)
-    - variables: inital genome and the tree
+- Dependencies
+    - transphylo: big tree where each node is a small coalescence tree
+    - alfsim: 
+	- fixed parameters (mutation rates, substitution model...etc)
+	- variables: inital genome and the tree
 - transphylo+alfsim:
-Create overall transmission tree and the within-host trees
-for the root (G0), 
-    1. extract the within-host tree
-    2. simulate with the initial genome and the within-host tree
-    3. extract the genome of offsprings
-    4. go to the offspring (breadth-first traversal) and repeat 1-3 until tips
+    stage 0. [TransPhylo] Create overall transmission tree and the within-host trees 
+    stage 1. Travel along the tree from the root (generation 0)
+	1. determine the tree topology
+	2. determine the initial genome, which is the starting genome for the origin and a simulated genome for the others
+	3. [ALF] simulate the within-host evolution with (1) and (2), and obtain the genomes at tips
+	    i. create the config files
+	    ii. run alfsim for simulating the coding regions
+	    iii. run dawg for simulating the integenic regions
+	4. map the simulated genomes, go to the offsprings, and repeat (1)-(3)
 
 ## Challenges
 - optimize the substitution rate (iteratively?)
