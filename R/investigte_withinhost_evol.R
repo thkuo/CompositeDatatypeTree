b83e1c57ab0d1a5f2cf54a56be94dd890e446f00
thkuo <tku16@helmholtz-hzi.de>
2020-04-06 16:39:14 +0200

    traceable wtrees

--- R/investigte_withinhost_evol.R
+++ R/investigte_withinhost_evol.R
@@ -10,19 +10,29 @@ pi=0.75
 dateT= 2008
 dateStartOutbreak=2005
 
-source('simulateOutbreak_unglued.R')
-t_w_trees <- simulateOutbreak_unglued(
+ctree<- simulateOutbreak(
     neg=neg,pi=pi,off.r=off.r,w.shape=w.shape,
     w.scale=w.scale,dateStartOutbreak=dateStartOutbreak,dateT=dateT)
-plotTTree(list(ttree= t_w_trees$ttree, nam= t_w_trees$nam), w.shape = w.shape, w.scale = w.scale)
-plotTTree2(list(ttree= t_w_trees$ttree, nam= t_w_trees$nam))
-source('TransPhylo/R/glueTrees.R')
-source('TransPhylo/R/computeHost.R')
-truth<-.glueTrees(t_w_trees$ttree,t_w_trees$wtrees)
-# truth[,1]<-truth[,1]+dateStartOutbreak
-ctree=list(ctree=truth,nam=mtt$nam,probttree=probttree,probwithin=probwithin)
-class(ctree)<-'ctree'
-plotCTree(ctree)
+
+source('wtreesFromCTree.R')
+wtrees<- wtreesFromCTree(ctree)
+
+source('phyloFromWTree_v2.R')
+w_phylos<-lapply(wtrees, function(wtree) phyloFromWTree(wtree))
+
+# source('simulateOutbreak_unglued.R')
+# t_w_trees <- simulateOutbreak_unglued(
+#     neg=neg,pi=pi,off.r=off.r,w.shape=w.shape,
+#     w.scale=w.scale,dateStartOutbreak=dateStartOutbreak,dateT=dateT)
+# plotTTree(list(ttree= t_w_trees$ttree, nam= t_w_trees$nam), w.shape = w.shape, w.scale = w.scale)
+# plotTTree2(list(ttree= t_w_trees$ttree, nam= t_w_trees$nam))
+# source('TransPhylo/R/glueTrees.R')
+# source('TransPhylo/R/computeHost.R')
+# truth<-.glueTrees(t_w_trees$ttree,t_w_trees$wtrees)
+# # truth[,1]<-truth[,1]+dateStartOutbreak
+# ctree=list(ctree=truth,nam=mtt$nam,probttree=probttree,probwithin=probwithin)
+# class(ctree)<-'ctree'
+# plotCTree(ctree)
 
 ## save the trees
 ## don't forget the node indices
@@ -35,9 +45,11 @@ write.table(t_w_trees$ttree, file = ttree_f, sep = '\t',  col.names = F)
 output_wtrees_d<- sprintf('%s/wtrees', output_d)
 dir.create(file.path(output_wtrees_d), recursive = T)   
 library(ape)
-source('phyloFromWTree.R')
-for (patient_ix in 1:length(t_w_trees$wtrees)){
-    dtr<- phyloFromWTree(t_w_trees$wtrees[[patient_ix]])
-    dtr_f<- sprintf('%s/%s.nwk', output_wtrees_d, patient_ix)
+## ori index as the tree names
+## so that the trees are traceable (i.e. tip of one to the origin of another)
+for (i in 1:length(w_phylos)){
+    dtr<- w_phylos[[i]]$phylo
+    dtree.name<- w_phylos[[i]]$ori
+    dtr_f<- sprintf('%s/%s.nwk', output_wtrees_d, dtree.name)
     write.tree(phy= dtr, file = dtr_f)
 }
--- R/phyloFromWTree_v2.R
+++ R/phyloFromWTree_v2.R
@@ -3,56 +3,78 @@
 #' @param wtree_from_ctree phylogenetic tree; col1: time; col2-3: indices of offsprings; col4: patien index; col5: node/tip indices
 #' @return phylo object
 #' @export
-phyloFromWTree <- function(wtree_from_ctree, root, root.edge.length) {
-  # nam=wtree$nam
-  # wtree=wtree$wtree
-  # 
-  # root<-which(wtree[1:(nrow(wtree)-1),1]==min(wtree[1:(nrow(wtree)-1),1]))
-  # root.edge.length<- wtree[root,1] - min(wtree[,1])
-  
-  # n<-ceiling(nrow(wtree)/2)
-  internal_ics<- which((wtree[1:(nrow(wtree)-1),2]!=0 | wtree[1:(nrow(wtree)-1),3]!=0))
-  tip_ics<- setdiff(1:(nrow(wtree)-1), internal_ics)
-  nam<- as.character(tip_ics)
-  n<- length(internal_ics)
-  
-  # if (n==1) return(ape::read.tree(text='(1);'))
-  if (length(tip_ics)==1) return(ape::read.tree(text=sprintf('(1:%f);', root.edge.length)))
-  tr<-list()
-  # tr$Nnode<-n-1
-  tr$Nnode<-n
-  tr$tip.label<-nam
-  # tr$edge<-matrix(0,nrow(wtree)-1,2)
-  tr$edge<-matrix(0,length(c(tip_ics, setdiff(internal_ics, root))),2)
-  # tr$edge.length<-rep(0,nrow(wtree)-1)
-  tr$edge.length<-rep(0,nrow(tr$edge))
-  # tr$edge<-matrix(0,n*2-2,2)
-  # tr$edge.length<-rep(0,n*2-2)
-  
-  iedge<-1
-  # root<-which(wtree[,1]==min(wtree[,1]))
-  tra<-c(1:length(tip_ics),root,setdiff(internal_ics,root))
-  tra2<-1:length(tra)
-  tra[tra]<-tra2
-  # for (i in (n+1):(2*n-1)) {
-  for (i in internal_ics){
-      for (offspring_ix in wtree[i,c(2,3)]){
-          if (offspring_ix != 0){
-              tr$edge[iedge,]<-c(tra[i],tra[offspring_ix])
-              tr$edge.length[iedge]<-wtree[offspring_ix,1]-wtree[i,1]
-              iedge<-iedge+1
-          }
-      } 
-    # tr$edge[iedge,]<-c(tra[i],tra[wtree[i,2]])
-    # tr$edge.length[iedge]<-wtree[wtree[i,2],1]-wtree[i,1]
-    # iedge<-iedge+1
-    # tr$edge[iedge,]<-c(tra[i],tra[wtree[i,3]])
-    # tr$edge.length[iedge]<-wtree[wtree[i,3],1]-wtree[i,1]
-    # iedge<-iedge+1
-  }
-  tr$root.time=min(wtree[,1])
-  tr$root.edge<- root.edge.length
-  class(tr)<-'phylo'
-  tr$node.label<- ((Ntip(tr)+1):(Ntip(tr)+Nnode(tr)))
-  return(tr)
+phyloFromWTree <- function(wtree_from_ctree) {
+    wtree<- wtree_from_ctree
+    # nam=wtree$nam
+    # wtree=wtree$wtree
+    # 
+    # n<-ceiling(nrow(wtree)/2)
+    # internal_ics<- which((wtree[1:(nrow(wtree)-1),2]!=0 | wtree[1:(nrow(wtree)-1),3]!=0))
+    # tip_ics<- setdiff(wtree[1:(nrow(wtree)-1),5], internal_ics)
+    
+    tip_ics<-which((!wtree[,2] %in% wtree[,5]) & (!wtree[,3] %in% wtree[,5]))
+    internal_ics<- setdiff(1:(nrow(wtree)-1), tip_ics)
+    nam<- as.character(wtree[tip_ics, 5])
+    node.nam<- as.character(wtree[internal_ics, 5])
+    n<- length(internal_ics)
+    
+    root<-which(wtree[1:(nrow(wtree)-1),1]==min(wtree[1:(nrow(wtree)-1),1]))
+    root.edge.length<- wtree[root,1] - min(wtree[,1])
+    
+    # if (n==1) return(ape::read.tree(text='(1);'))
+    if (length(tip_ics)==1) {
+        ori<- wtree[nrow(wtree), 5]
+        ori.time<- wtree[nrow(wtree), 1]
+        root.edge.length<- ori.time - wtree[root,1]
+        tr<- ape::read.tree(text=sprintf('(%s:%f);', tip_ics[1] , root.edge.length))
+        tr$root.edge<- root.edge.length
+        return(list(phylo= tr, ori= ori, ori.time= ori.time))
+    }
+    
+    tr<-list()
+    # tr$Nnode<-n-1
+    tr$Nnode<-n
+    tr$abel<-nam
+    # tr$edge<-matrix(0,nrow(wtree)-1,2)
+    # except for the root
+    tr$edge<-matrix(0,length(c(tip_ics, setdiff(internal_ics, root))),2)
+    # tr$edge.length<-rep(0,nrow(wtree)-1)
+    tr$edge.length<-rep(0,nrow(tr$edge))
+    # tr$edge<-matrix(0,n*2-2,2)
+    # tr$edge.length<-rep(0,n*2-2)
+    
+    iedge<-1
+    # root<-which(wtree[,1]==min(wtree[,1]))
+    tra<-c(1:length(tip_ics),root,setdiff(internal_ics,root))
+    tra2<-1:length(tra)
+    tra[tra]<-tra2
+    # for (i in (n+1):(2*n-1)) {
+    # for (i in internal_ics){
+    for (i in internal_ics){
+        cur_ix<- internal_ics[i]
+        for (offspring_ix in wtree[i, c(2,3)]){
+            if (offspring_ix != 0){
+                # tr$edge[iedge,]<-c(tra[i],tra[offspring_ix])
+                tr$edge[iedge,]<-c(tra[i],tra[which(wtree[,5] == offspring_ix)])
+                tr$edge.length[iedge]<-wtree[which(wtree[,5] == offspring_ix),1]-wtree[i,1]
+                iedge<-iedge+1
+            }
+        } 
+        # tr$edge[iedge,]<-c(tra[i],tra[wtree[i,2]])
+        # tr$edge.length[iedge]<-wtree[wtree[i,2],1]-wtree[i,1]
+        # iedge<-iedge+1
+        # tr$edge[iedge,]<-c(tra[i],tra[wtree[i,3]])
+        # tr$edge.length[iedge]<-wtree[wtree[i,3],1]-wtree[i,1]
+        # iedge<-iedge+1
+    }
+    # tr$root.time=min(wtree[,1])
+    tr$root.edge<- root.edge.length
+    class(tr)<-'phylo'
+    tr$tip.label<- wtree[tip_ics, 5]
+    tr$node.label<- wtree[internal_ics, 5]
+    
+    ori<- wtree[nrow(wtree), 5]
+    ori.time<- wtree[nrow(wtree), 1]
+    
+    return(list(phylo= tr, ori= ori, ori.time= ori.time))
 }
