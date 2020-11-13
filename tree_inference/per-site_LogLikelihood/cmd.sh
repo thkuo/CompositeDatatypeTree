94058bb39c6bce980601e7c9b04b350ee364dc77
thkuo <thkuo@titan-compute-01.bifo.helmholtz-hzi.de>
2020-11-13 16:37:41 +0100

    include computation of log-likelihood scores

---
+++ tree_inference/per-site_LogLikelihood/cmd.sh
@@ -0,0 +1,10 @@
+source activate cdtree_env
+export cpu_num=20
+export config_f=config.yml
+
+snakemake -p \
+  --rerun-incomplete --use-conda \
+ --conda-prefix /net/metagenomics/data/from_moni/old.tzuhao/TreePaper/shared_envs \
+ -j  $cpu_num \
+ --configfile=$config_f \
+ --snakefile=./compute_psll.smk 
